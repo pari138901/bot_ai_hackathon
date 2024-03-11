e0ac23516a5bec06611a34108cd489a43356a624
pgandotra <pgandotra_contractor@unboxedtechnology.com>
2024-03-11 14:08:24 -0400

    Added ML model and intent matching updates

--- Bot_StreamLit.py
+++ Bot_StreamLit.py
@@ -1,10 +1,13 @@
 from openai import AzureOpenAI
-import os
 import pyodbc
 import streamlit as st
 from streamlit_chat import message
 from config import DATABASE_CONFIG, a_endpoint, a_key, a_version
-
+from datetime import datetime
+from ml_model import call_ml_endpoint
+import json
+import pytz
+import random
 
 # Initialize session state keys if they don't exist
 if 'generated' not in st.session_state:
@@ -115,23 +118,117 @@ def generate_chat_response(sql_description):
     response = openai_completion(chat_prompt_response)
     return response
 
+def get_intent_sys_message():
+    
+    intent_description = """
+    I need your help in understanding the user intent when they ask for prompt. I have a sql server which I'm helping user with for reporting and an ML model that informs user
+    how long it will take them to travel in New York city. Based on your reply I would understand if i need to invoke the ml model or my sql server.
+    If a user asks how long it will take them to travel between 2 locations you need to create a reply 
+    in this format:
+    intent:'ml_model'
+    if the user asks any query where we might involve the following schema
+    The 'dbo.vw_trips' table contains trip data with the following columns:
+        TripID (unique identifier), PickupDateTime (pickup timestamp), 
+        PickupLocation_Zone (pickup zone), PickLocation_Borough (pickup borough),
+          DropoffDateTime (dropoff timestamp), DropOffLocation_Zone (dropoff zone), 
+          DropOffLocation_Borough (dropoff borough), TravelTime (duration), PassengerCount (number of passengers), 
+          FareAmount (fare), FeeAmount (additional fees), TotalAmount (total charged).
+    reply in this format:
+    intent:'sql'. 
+    Donot add anything else except the format.
+    """
+    add_to_chat('system',intent_description)
+
+def generate_payload(prompt):
+    ai_system_msg = []
+    data_payload = []
+    est = pytz.timezone('America/New_York')
+    now_est = datetime.now(est)
+    # Extracting the required values
+    month_num = now_est.month
+    day_of_week = now_est.isoweekday()  # This will be the full name of the day
+    hour_24 = now_est.hour
+    minute = now_est.minute
+    
+    data_payload.extend([month_num,day_of_week, hour_24, minute])
+
+    locationid_description = """
+    I need your help in generating the json payload based on the user prompt. I need the following information:
+    Pickup LocationID: if in the prompt user asks for pickup location 'Newark Airport', iD is 1. 
+    If the pickup location is 'Brooklyn Heights', iD is 33.
+    If the pickup location is 'Central Park', iD is 43.
+    If the pickup location is 'China Town', iD is 45.
+    If the pickup location is 'JFK Airport', iD is 132.
+    If the pickup location is 'SoHo', iD is 211.
+    Dropoff LocationID: if in the prompt user asks for dropoff location 'Newark Airport', iD is 1. 
+    If the Dropoff location is 'Brooklyn Heights', iD is 33.
+    If the Dropoff location is 'Central Park', iD is 43.
+    If the Dropoff location is 'China Town', iD is 45.
+    If the Dropoff location is 'JFK Airport', iD is 132.
+    If the Dropoff location is 'SoHo', iD is 211.
+    Calculate and fill the result in this format: [Pickup LocationID,Dropoff LocationID ]
+    Don't reply anything else except the format. Make sure to only include the values in the list format and maintain the sort order.
+    """
+    ai_system_msg.append({'role': 'system', 'content': locationid_description})
+    ai_system_msg.append({'role': 'user', 'content': prompt})
+
+    # Asking on the location IDs
+    response_locationid = openai_completion(ai_system_msg)
+
+    locationids= json.loads(response_locationid)
+    data_payload.extend(locationids) 
+
+    # the Trip Distance
+    dist = round(random.uniform(17,25),1)
+    data_payload.extend([dist])
+
+    return data_payload
+
 def main(user_input):
      # Add user input to session state for display
     st.session_state['past'].append(user_input)
-    add_to_chat('user',user_input)
-    
-    query_response = openai_completion(st.session_state['messages'], temperature=0.0, max_tokens=1500)
-    st.write(query_response)
-    query = process_query(query_response)
 
-    sql_result = execute_sql_query(query)
+    # Adding user prompt to the list
+    add_to_chat('user',user_input)
+    get_intent_sys_message()
+    intent_response = openai_completion(st.session_state['messages'], temperature=0.0, max_tokens=1500)
+    if "intent:'sql'" in intent_response:
+        generate_sql_sys_msg()
+        query_response = openai_completion(st.session_state['messages'], temperature=0.0, max_tokens=1500)
+        st.write(query_response)
+        query = process_query(query_response)
+        sql_result = execute_sql_query(query)
+        formatted_result = ', '.join([str(row) for row in sql_result])
+        response_description = f"The user query was: {user_input}. The result of the query is: {formatted_result}."
+    elif "intent:'ml_model'" in intent_response:
+        payload_data = generate_payload(user_input)
+        INPUT_DATA = {
+            "input_data": {
+            "columns": [
+                "PickupMonth",
+                "PickupDayOfWeek",
+                "PickupHour",
+                "PickupMinute",
+                "PickupLocationID",
+                "DropoffLocationID",
+                "TripDistance"
+            ],
+            "index": [0],
+            "data": []
+            }
+        }
+        payload = INPUT_DATA
+        payload['input_data']['data'] =  [payload_data]
+        json_payload = json.dumps(payload)
+        result = call_ml_endpoint(json_payload)
+        result_num = result.decode("utf-8")
+        response_description = f"The user query was: {user_input}. The result of the query is: {result_num}."
     
-    formatted_result = ', '.join([str(row) for row in sql_result])
-    sql_description = f"The user query was: {user_input}. The result of the query is: {formatted_result}."
-    
-    chat_response = generate_chat_response(sql_description)
-    # st.session_state['messages'].append({"role": "assistant", "content": chat_response})
+    chat_response = generate_chat_response(response_description)
     st.session_state['generated'].append(chat_response)
+    
+    # query_response = openai_completion(st.session_state['messages'], temperature=0.0, max_tokens=1500)
+    
     # print("Response: ", chat_response)
 
     # for message in st.session_state['messages']:
@@ -143,7 +240,6 @@ with st.container():
     submit_button = st.button("Submit")
 
 if submit_button and user_input:
-    generate_sql_sys_msg()
     main(user_input)
 
 # Displaying the chat history
--- ml_model.py
+++ ml_model.py
@@ -12,7 +12,7 @@ def call_ml_endpoint(data):
     if True and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
         ssl._create_default_https_context = ssl._create_unverified_context
       
-    body = str.encode(json.dumps(data))  
+    body = str.encode(data)  
   
     url = ml_url
       
@@ -27,9 +27,15 @@ def call_ml_endpoint(data):
     try:  
         response = urllib.request.urlopen(req)  
         result = response.read()  
-        print(result)
+        return result
 
     except urllib.error.HTTPError as error:  
         print("The request failed with status code: " + str(error.code))  
         print(error.info())  
         print(error.read().decode("utf8", 'ignore'))  
+
+payload = json.dumps({'input_data': {'columns': ['PickupMonth', 'PickupDayOfWeek', 'PickupHour', 'PickupMinute', 'PickupLocationID', 'DropoffLocationID', 'TripDistance'], 'index': [0], 'data': [[3, 1, 13, 45, 45, 132, 24.7]]}})
+res = call_ml_endpoint(payload)
+# type(res)
+# res2 = res.decode("utf-8")
+# res2
